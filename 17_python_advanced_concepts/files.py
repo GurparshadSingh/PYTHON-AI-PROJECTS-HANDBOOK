2e0fea60036109677085c03fe9a0210276ec55ce
Gurparshad Singh <gurparshad0878@gmail.com>
2026-06-09 10:32:02 +0530

    Add files via upload

---
+++ 17_python_advanced_concepts/files.py
@@ -0,0 +1,21 @@
+# f=open("file.txt", "w")
+# f.write("\nHello, this is a sample text file.\n")
+# f.write("This file is used to demonstrate file handling in Python.\n")
+# f.writelines(["Line 1: This is the first line.\n", "Line 2: This is the second line.\n"])
+# f.close()
+# f=open("file.txt", "w")
+# f.write("\ngfdlsinugodfsngodfsnigiop\n")
+# # f.writelines(["nodgfvsnijodfvsnhnhdfvbuidfvsbkj.\n", "adcjhidfcshdfcshhb.\n"])
+# f.close()
+
+
+# f=open("file.txt", "r")
+# content = f.read()
+# print(content)
+# f.close()
+
+
+
+nums = [1, 2, 3, 4, 5]
+it=iter(nums)
+print(next(it))
